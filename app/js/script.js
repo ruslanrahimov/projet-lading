108852d452f348e092330e3e434408e12f5ad5a3
Ruslan <rahimoff.tr@gmail.com>
2021-03-18 22:53:12 +0300

    animation fixed

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,32 @@
+let swiperAnimation = new SwiperAnimation();
 let swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 5000,
-      },
-      effect: 'fade',
-      fadeEffect: {
-        crossFade: true
-      },
-   
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<div class="indicator"><span class="' + className + '">' + (`0${index + 1}`) + '</span></div>';
-          },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<div class="indicator"><span class="' + className + '">' + (`0${index + 1}`) + '</span></div>';
     },
+  },
+  on: {
+    init: function () {
+      swiperAnimation.init(this).animate();
+    },
+    slideChange: function () {
+      swiperAnimation.init(this).animate();
+    }
+  },
+  
 });
+
+
+
+
 
 
 
