07c6b281ad583b9d7ec04ad655b7ec7eecaed3e7
Ruslan <rahimoff.tr@gmail.com>
2021-03-22 01:05:53 +0300

    solut

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,7 @@ let swiperAnimation = new SwiperAnimation();
 
 const sliderFirst = document.querySelector('.swiper-container');
 const sliderSecond = document.querySelector('.swiper-container-2');
+const sliderThird = document.querySelector('.swiper-container-3');
 
 
 let swiper = new Swiper(sliderFirst, {
@@ -42,6 +43,25 @@ let swiperSecond = new Swiper(sliderSecond, {
   },
 
 });
+
+let swiperThird = new Swiper(sliderThird, {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.solutions-slider-next',
+    prevEl: '.solutions-slider-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination-3',
+    type: 'bullets',
+  },
+  autoplay: {
+    delay: 8000,
+  },
+})
+
 
 // Accordion //
 
