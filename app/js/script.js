472452af093c7e80138653861358707cb47aee44
Ruslan <rahimoff.tr@gmail.com>
2021-03-28 21:58:41 +0300

    not fixed

--- app/js/script.js
+++ app/js/script.js
@@ -1,69 +1,67 @@
+document.addEventListener('DOMContentLoaded', function () {
 
-let swiperAnimation = new SwiperAnimation();
+  const sliderFirst = document.querySelector('.swiper-container');
+  const sliderSecond = document.querySelector('.swiper-container-2');
+  const sliderThird = document.querySelector('.swiper-container-3');
 
-const sliderFirst = document.querySelector('.swiper-container');
-const sliderSecond = document.querySelector('.swiper-container-2');
-const sliderThird = document.querySelector('.swiper-container-3');
+  
+  
 
-let swiper = new Swiper(sliderFirst, {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  autoplay: {
-    delay: 10000,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: false,
-    renderBullet: function (index, className) {
-      return '<div class="indicator"><span class="' + className + '">' + (`0${index + 1}`) + '</span></div>';
+  let swiper = new Swiper(sliderFirst, {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 10000,
     },
-  },
-  on: {
-    init: function () {
-      swiper.init(this).animate();
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: false,
+      renderBullet: function (index, className) {
+        return '<div class="indicator"><span class="' + className + '">' + (`0${index + 1}`) + '</span></div>';
+      },
     },
-    slideChange: function () {
-      swiper.init(this).animate();
-    }
-  },
 
-});
+  });
 
-let swiperSecond = new Swiper(sliderSecond, {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
-  },
-  autoplay: {
-    delay: 8000,
-  },
 
-});
 
-let swiperThird = new Swiper(sliderThird, {
-  direction: 'horizontal',
-  autoHeight: true,
-  loop: true,
-  slidesPerView: 4,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: '.solutions-slider-next',
-    prevEl: '.solutions-slider-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination-3',
-    type: 'bullets',
-    dynamicBullets: true,
+  let swiperSecond = new Swiper(sliderSecond, {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+    autoplay: {
+      delay: 8000,
+    },
 
-  },
-  autoplay: {
-    delay: 3000,
-  },
+  });
+
+  let swiperThird = new Swiper(sliderThird, {
+    direction: 'horizontal',
+    autoHeight: true,
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.solutions-slider-next',
+      prevEl: '.solutions-slider-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination-3',
+      type: 'bullets',
+      dynamicBullets: true,
+
+    },
+    autoplay: {
+      delay: 3000,
+    },
+  })
 })
+
 
 
 
@@ -83,7 +81,7 @@ document.querySelector('.service-accordion').addEventListener('click', (event) =
 window.addEventListener('scroll', function () {
   const nav = document.querySelector('.header__nav')
   const windowPosition = window.scrollY > 0;
-  
+
   nav.classList.toggle('header__nav--active', windowPosition)
 })
 
@@ -98,11 +96,11 @@ const burger = document.querySelector('.header__menu-burger')
 burger.addEventListener('click', (e) => {
   e.preventDefault()
   header.classList.toggle('open')
-  
-  if(header.classList.contains('open')){
+
+  if (header.classList.contains('open')) {
     menuList.classList.add('nav__mobile-menu--active')
     body.classList.add('noscroll')
-  }else{
+  } else {
     menuList.classList.remove('nav__mobile-menu--active')
     body.classList.remove('noscroll')
   }
@@ -116,7 +114,7 @@ links.forEach(link => link.addEventListener('click', function () {
   links.forEach(activeLink => activeLink.classList.remove('nav__link--active'))
   this.classList.add('nav__link--active')
 
-  if(menuList.classList.contains('nav__mobile-menu--active')){
+  if (menuList.classList.contains('nav__mobile-menu--active')) {
     header.classList.remove('open')
     menuList.classList.remove('nav__mobile-menu--active')
   }
