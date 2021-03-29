996895ce7431d7df12d6fdd12615110483ca01be
Ruslan <rahimoff.tr@gmail.com>
2021-03-29 04:13:18 +0300

    modal windows is finished

--- app/js/script.js
+++ app/js/script.js
@@ -122,6 +122,35 @@ links.forEach(link => link.addEventListener('click', function () {
   }
 }))
 
+// Modul Windows
+
+const cards = document.querySelectorAll('.solutions__card');
+const modalOverlay = document.querySelector('.modal-overlay ');
+const modals = document.querySelectorAll('.modal');
+
+cards.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		let path = e.currentTarget.getAttribute('data-path');
+
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+
+		document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+		modalOverlay.classList.add('modal-overlay--visible');
+	});
+});
+
+modalOverlay.addEventListener('click', (e) => {
+
+	if (e.target == modalOverlay) {
+		modalOverlay.classList.remove('modal-overlay--visible');
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+	}
+});
+
 
 
 
